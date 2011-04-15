cd2234b55981b5cd406fe38f5d3e3da382928a35
Nicolas Paris <nicolas.caen@gmail.com>
2011-04-15 04:00:23 +0200

    plugins added

---
+++ vim/template/php.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * (c) 2011 Nicolas Paris <nicolas.caen@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
